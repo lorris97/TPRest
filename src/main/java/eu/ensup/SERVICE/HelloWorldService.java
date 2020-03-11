ada520934cfa8e5ef411c4a4a25cd787815ad103
lorris97 <33892942+lorris97@users.noreply.github.com>
2020-03-11 17:30:40 +0100

    Add files via upload

---
+++ src/main/java/eu/ensup/SERVICE/HelloWorldService.java
@@ -0,0 +1,24 @@
+package eu.ensup.SERVICE;
+
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+@Path("/hello")
+public class HelloWorldService {
+    
+    
+    @GET
+    @Path("/{param}")
+    public Response getMsg(@PathParam("param") String msg)
+    {
+        
+        String outpout = "Jersey repond via OBJIS " + msg ;
+        
+        return Response.status(200).entity(outpout).build();
+        
+    }
+    
+}
